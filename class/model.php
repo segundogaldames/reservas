667173b894d67d9369852854601647ff63f32c2b
Segundo <segundogaldames@gmail.com>
2021-10-07 22:46:08 -0300

    Creacion de model y rolModel

---
+++ class/model.php
@@ -0,0 +1,14 @@
+<?php
+#llamada al archivo de conexion de la base de datos
+require('conexion.php');
+
+class Model
+{
+    protected $_db;
+
+    public function __construct()
+    {
+        #creacion de una instancia de la clase conexion
+        $this->_db = new Conexion;
+    }
+}
---
+++ class/rolModel.php
@@ -0,0 +1,9 @@
+<?php
+#llamada a la clase model
+require_once('model.php');
+
+class RolModel extends Model
+{
+    #la funcion de esta clase sera trabajar directamente con la tabla roles
+}
+
