41eb11315f426c1190d9a3c00cde30e7964f038c
Segundo <segundogaldames@gmail.com>
2021-10-07 22:26:33 -0300

    Modificacion de parametros de conexion DB

--- class/conexion.php
+++ class/conexion.php
@@ -5,7 +5,7 @@ class Conexion extends PDO
 {
         //atributos de la clase conexion
 	private $host = 'localhost';
-	private $base = 'miprimersitio';
+	private $base = 'reservas';
 	private $user = 'root';
 	private $pass = '1234'; //el password para usuarios de Windows es vacio ''
 	private $charset = 'utf8';
@@ -13,13 +13,14 @@ class Conexion extends PDO
         //constructor de la clase conexion
         //los constructores de clase permiten inicializar por defecto atributos y/o metodos de una clase
 	public function __construct(){
-                //constructor de clase padre llamada PDO
-                parent::__construct(
-                        'mysql:host=' . $this->host . ';dbname=' . $this->base,
-                        $this->user,
-                        $this->pass,
-                        array(
-                                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . $this->charset
-                        ));
-        }
+        //constructor de clase padre llamada PDO
+        parent::__construct(
+            'mysql:host=' . $this->host . ';dbname=' . $this->base,
+            $this->user,
+            $this->pass,
+            array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . $this->charset
+            )
+        );
+    }
 }
