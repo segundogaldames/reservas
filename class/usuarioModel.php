740e82fa3239579f81cd2ea48c7d321c9747c29c
Segundo <segundogaldames@gmail.com>
2021-11-04 22:05:59 -0300

    Edicion o cambio de password

--- class/usuarioModel.php
+++ class/usuarioModel.php
@@ -11,6 +11,15 @@ class UsuarioModel extends Model
         parent::__construct();
     }
 
+    public function getUsuarioId($id)
+    {
+        $usu = $this->_db->prepare("SELECT u.id, u.activo, u.empleado_id, u.created_at, u.updated_at, e.nombre as empleado FROM usuarios u INNER JOIN empleados e ON u.empleado_id = e.id WHERE u.id = ?");
+         $usu->bindParam(1, $id);
+        $usu->execute();
+
+        return $usu->fetch();
+    }
+
     public function getUsuarioEmpleado($empleado)
     {
         $usu = $this->_db->prepare("SELECT id FROM usuarios WHERE empleado_id = ?");
@@ -27,6 +36,20 @@ class UsuarioModel extends Model
         $usu = $this->_db->prepare("INSERT INTO usuarios(clave, activo, empleado_id, created_at, updated_at) VALUES(?, 1, ?, now(), now())");
         $usu->bindParam(1, $clave);
         $usu->bindParam(2, $empleado);
+        $usu->execute();
+
+        $row = $usu->rowCount();
+
+        return $row;
+    }
+
+    public function editPassword($id, $clave)
+    {
+        $clave = Hash::getHash('sha1', $clave, HASH_KEY);
+
+        $usu = $this->_db->prepare("UPDATE usuarios SET clave = ?, updated_at = now() WHERE id = ?");
+        $usu->bindParam(1, $clave);
+        $usu->bindParam(2, $id);
         $usu->execute();
 
         $row = $usu->rowCount();
---
+++ usuarios/editPassword.php
@@ -0,0 +1,102 @@
+<?php
+    #instrucciones que nos permiten ver errores en tiempos de ejecucion
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    #llamada al archivo que contiene las rutas del sistema
+    require('../class/rutas.php');
+    require('../class/config.php');
+    require('../class/usuarioModel.php');
+
+    session_start();
+
+    $usuarios = new UsuarioModel;
+
+    $title = 'Cambiar Password';
+
+    if (isset($_GET['usuario'])) {
+        $id = (int) $_GET['usuario'];
+
+        $usuario = $usuarios->getUsuarioId($id);
+
+        if (isset($_POST['confirm']) && $_POST['confirm'] == 1) {
+
+            $clave = trim(strip_tags($_POST['clave'])); //sanitizacion basica
+            $reclave = trim(strip_tags($_POST['reclave']));
+
+            if (empty($clave) || strlen($clave) < 8) {
+                $msg = 'Ingrese el password de la cuenta con al menos 8 caracteres';
+            }elseif ($reclave != $clave) {
+                $msg = 'Los passwords ingresados no coinciden';
+            }else{
+                $usu = $usuarios->editPassword($id, $clave);
+
+                if ($usu) {
+                    $_SESSION['success'] = 'El password se ha modificado correctamente';
+                    header('Location: ' . SHOW_EMPLEADO . $usuario['empleado_id']);
+                }
+            }
+
+            //print_r($nombre);
+        }
+
+    }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo TITLE . $title ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+    <script src="../js/funciones.js"></script>
+</head>
+<body>
+    <header>
+        <!-- llamada a archivo de menu -->
+        <?php include('../partials/menu.php'); ?>
+    </header>
+    <div class="container-fluid">
+        <div class="col-md-6 offset-md-3">
+            <h4><?php echo $title; ?></h4>
+
+            <p class="text-danger">Campos obligatorios *</p>
+
+            <?php if(isset($msg)): ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $msg; ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if($usuario): ?>
+                <form name="form" action="" method="post">
+                    <div class="mb-3">
+                        <label for="clave" class="form-label">Empleado: <?php echo $usuario['empleado']; ?> </label>
+                    </div>
+                    <div class="mb-3">
+                        <label for="clave" class="form-label">Password<span class="text-danger">*</span>  </label>
+                        <input type="password" name="clave" class="form-control" oncopy="return false" onpaste="return false" aria-describedby="usuarioHelpInline">
+                        <div id="usuarioHelp" class="form-text text-danger">Ingrese el password del Empleado</div>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="reclave" class="form-label">Confirmar Password<span class="text-danger">*</span>  </label>
+                        <input type="password" name="reclave" class="form-control" oncopy="return false" onpaste="return false" aria-describedby="usuarioHelpInline">
+                        <div id="usuarioHelp" class="form-text text-danger">Confirme el password del Empleado</div>
+                    </div>
+                    <div class="mb-3">
+                        <input type="hidden" name="confirm" value="1">
+                        <button type="submit" class="btn btn-outline-success">Editar</button>
+                        <a href="<?php echo SHOW_EMPLEADO . $usuario['empleado_id']; ?>" class="btn btn-outline-primary">Volver</a>
+                    </div>
+                </form>
+            <?php else: ?>
+                <p class="text-info">El password no pudo ser modificado</p>
+            <?php endif; ?>
+        </div>
+    </div>
+</body>
+</html>
