4e06bdacf720c69c60f203bafe052101e9834fc2
Segundo <segundogaldames@gmail.com>
2021-11-20 15:06:36 -0300

    respaldo BD

--- reservas.sql
+++ reservas.sql
@@ -125,6 +125,37 @@ INSERT INTO `pacientes` VALUES (1,'16800356-9','Raul Mora Jimenez','rmora@gmail.
 UNLOCK TABLES;
 
 --
+-- Table structure for table `reservas`
+--
+
+DROP TABLE IF EXISTS `reservas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `activo` int NOT NULL,
+  `especialidad_id` int NOT NULL,
+  `paciente_id` int NOT NULL,
+  `usuario_id` int NOT NULL,
+  `horario_id` int NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservas`
+--
+
+LOCK TABLES `reservas` WRITE;
+/*!40000 ALTER TABLE `reservas` DISABLE KEYS */;
+INSERT INTO `reservas` VALUES (1,'2021-11-21',1,2,1,2,3,'2021-11-20 14:02:34','2021-11-20 14:02:34');
+/*!40000 ALTER TABLE `reservas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -211,4 +242,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-18 22:48:52
+-- Dump completed on 2021-11-20 15:06:12
